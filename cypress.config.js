bfed0c439bc70f5fa353c9f9a92ad50a5012638b
Vlad Mocan <vladcatalin.mocan@gmail.com>
2022-10-07 11:36:26 +0300

    update to cypress 10+

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+  screenshotOnFailure: true,
+  screenshotsFolder: 'mochawesome-report/assets',
+  video: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://be-the-qa-fe.herokuapp.com',
+    excludeSpecPattern: '**/examples/**',
+  },
+})
